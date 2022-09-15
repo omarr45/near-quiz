e308b3df9eed9b0bbf6ec5761e6e51d9136fe77d
omarr45 <58887202+omarr45@users.noreply.github.com>
2022-09-15 12:06:24 +0200

    added questions context

--- src/components/AddQuestion.js
+++ src/components/AddQuestion.js
@@ -1,20 +1,33 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useContext } from 'react';
+
+import { QuestionsContext } from './utils/questionsContext';
 import { v4 as uuidv4 } from 'uuid';
 
-const AddQuestion = ({ n = 1 }) => {
-  const [options, setOptions] = useState([
-    { id: uuidv4(), option: '', isCorrect: true },
-    { id: uuidv4(), option: '', isCorrect: false },
-  ]);
+const AddQuestion = ({ n = 1, id }) => {
+  const { quizQuestions, setQuizQuestions } = useContext(QuestionsContext);
 
   return (
-    <div className='p-4 rounded-md  space-y-3 bg-gray-200 dark:bg-gray-800'>
-      <label
-        htmlFor={'q' + ('0' + n).slice(-2)}
-        className='block mb-2 text-md font-semibold text-gray-900 dark:text-white'>
-        Question {n}
-      </label>
+    <div className='p-4 rounded-md space-y-3 bg-gray-200 dark:bg-gray-800'>
+      <div className='flex items-center justify-between'>
+        <label
+          htmlFor={'q' + ('0' + n).slice(-2)}
+          className='block mb-2 text-md font-semibold text-gray-900 dark:text-white'>
+          Question {n}
+        </label>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            // const newQuestions = quizQuestions.filter((q) => q.id !== id);
+            // setQuizQuestions(
+            //   newQuestions.map((q, index) => {
+            //     return { ...q, id: index + 1 };
+            //   })
+            // );
+          }}
+          className='line-through ml-auto text-red-700 bg-transparent border-2 border-red-700 hover:bg-red-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-300 rounded-md font-semibold text-sm p-2.5 text-center inline-flex items-center justify-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800'>
+          Remove Question
+        </button>
+      </div>
       <input
         type='text'
         name={'question ' + n}
@@ -30,16 +43,27 @@ const AddQuestion = ({ n = 1 }) => {
           className='h-8 w-8 text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium  rounded-full text-sm p-2.5 text-center inline-flex items-center justify-center ml-2 dark:bg-green-500 dark:hover:bg-green-600 dark:focus:ring-green-800'
           onClick={(e) => {
             e.preventDefault();
-            setOptions([
-              ...options,
-              { id: uuidv4(), option: '', isCorrect: false },
-            ]);
+            // add another option to the question with the same id
+            setQuizQuestions((prev) => {
+              return prev.map((question) => {
+                if (question.id === id) {
+                  return {
+                    ...question,
+                    options: [
+                      ...question.options,
+                      { id: uuidv4(), option: '', isCorrect: false },
+                    ],
+                  };
+                }
+                return question;
+              });
+            });
           }}>
           <span className='text-lg'>+</span>
         </button>
       </p>
 
-      {options.map((option) => (
+      {/* {options.map((option) => (
         <div className='flex items-center' key={uuidv4()}>
           <input
             checked={option.isCorrect}
@@ -91,7 +115,101 @@ const AddQuestion = ({ n = 1 }) => {
             <span>X</span>
           </button>
         </div>
-      ))}
+      ))} */}
+      {quizQuestions.map((question) => {
+        if (question.id === id) {
+          return question.options.map((option) => {
+            return (
+              <div className='flex items-center' key={uuidv4()}>
+                <input
+                  checked={option.isCorrect}
+                  onChange={(e) => {
+                    setQuizQuestions((prev) => {
+                      return prev.map((question) => {
+                        if (question.id === id) {
+                          return {
+                            ...question,
+                            options: question.options.map((opt) => {
+                              if (opt.id === option.id) {
+                                return {
+                                  ...opt,
+                                  isCorrect: e.target.checked,
+                                };
+                              }
+                              return opt;
+                            }),
+                          };
+                        }
+                        return question;
+                      });
+                    });
+                  }}
+                  id={'bordered-radio-' + option.id}
+                  type='radio'
+                  value=''
+                  name={'bordered-radio-q' + n}
+                  className='w-4 h-4 text-primary-600 bg-gray-100 border-gray-300 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+                />
+                <label
+                  htmlFor={'bordered-radio-' + option.id}
+                  className='ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300'>
+                  <input
+                    type='text'
+                    placeholder={'New Option'}
+                    onChange={(e) => {
+                      setQuizQuestions((prev) => {
+                        return prev.map((question) => {
+                          if (question.id === id) {
+                            return {
+                              ...question,
+                              options: question.options.map((opt) => {
+                                if (opt.id === option.id) {
+                                  return {
+                                    ...opt,
+                                    option: e.target.value,
+                                  };
+                                }
+                                return opt;
+                              }),
+                            };
+                          }
+                          return question;
+                        });
+                      });
+                    }}
+                    className='pl-4 rounded border w-full border-gray-200 dark:border-gray-700 focus:ring-primary-500 focus:border-primary-500'
+                  />
+                </label>
+                <button
+                  type='button'
+                  value={option.option}
+                  className='h-8 w-9 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium  rounded-full text-sm p-2.5 text-center inline-flex items-center justify-center ml-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800'
+                  onClick={
+                    question.options.length > 2
+                      ? () => {
+                          setQuizQuestions((prev) => {
+                            return prev.map((question) => {
+                              if (question.id === id) {
+                                return {
+                                  ...question,
+                                  options: question.options.filter(
+                                    (opt) => opt.id !== option.id
+                                  ),
+                                };
+                              }
+                              return question;
+                            });
+                          });
+                        }
+                      : () => {}
+                  }>
+                  <span>X</span>
+                </button>
+              </div>
+            );
+          });
+        }
+      })}
     </div>
   );
 };
--- src/components/CreateQuiz.js
+++ src/components/CreateQuiz.js
@@ -1,108 +1,121 @@
 import AddQuestion from './AddQuestion';
+import { QuestionsContext } from './utils/questionsContext';
 import React from 'react';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const CreateQuiz = () => {
   const [questions, setQuestions] = useState(1);
+  const [quizQuestions, setQuizQuestions] = useState([
+    {
+      id: 1,
+      question: '',
+      options: [
+        { id: uuidv4(), option: '', isCorrect: true },
+        { id: uuidv4(), option: '', isCorrect: false },
+      ],
+    },
+  ]);
 
   return (
     <main className='flex flex-col items-center justify-between bg-white dark:bg-gray-900 dark:text-white my-5 px-6 mx-auto max-w-4xl text-center lg:p-12 lg:pt-8'>
       <h1 className='mb-8 text-2xl font-bold leading-tight tracking-tight text-gray-900 md:text-4xl dark:text-white'>
         Create a new Quiz
       </h1>
-      <div className='flex flex-col items-start justify-between gap-0 w-full  md:gap-8'>
-        {/* Left Section */}
-        <div className='flex flex-col items-center justify-center text-left px-6 py-8 mx-auto w-full lg:p-0'>
-          <form
-            className='space-y-2 md:space-y-4 w-full'
-            action='#'
-            autoComplete='false'>
-            <div>
-              <label
-                htmlFor='title'
-                className='block mb-2 text-md font-medium text-gray-900 dark:text-white'>
-                Title
-              </label>
-              <input
-                type='text'
-                name='title'
-                id='title'
-                className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
-                placeholder='Quiz Title'
-                required=''
-              />
-            </div>
-            <div>
-              <label
-                htmlFor='description'
-                className='block mb-2 text-md font-medium text-gray-900 dark:text-white'>
-                Description
-              </label>
-              <textarea
-                type='text'
-                name='description'
-                id='description'
-                placeholder='Quiz Description'
-                className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
-                required=''
-              />
-            </div>
-            <div className='flex gap-4 w-full'>
-              <div className='w-full'>
+      <QuestionsContext.Provider value={{ quizQuestions, setQuizQuestions }}>
+        <div className='flex flex-col items-start justify-between gap-0 w-full  md:gap-8'>
+          {/* Left Section */}
+          <div className='flex flex-col items-center justify-center text-left px-6 py-8 mx-auto w-full lg:p-0'>
+            <form
+              className='space-y-2 md:space-y-4 w-full'
+              action='#'
+              autoComplete='false'>
+              <div>
                 <label
-                  htmlFor='winners'
+                  htmlFor='title'
                   className='block mb-2 text-md font-medium text-gray-900 dark:text-white'>
-                  Number of Winners
+                  Title
                 </label>
                 <input
-                  type='number'
-                  min={0}
-                  name='winners'
-                  id='winners'
-                  placeholder='5'
+                  type='text'
+                  name='title'
+                  id='title'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
+                  placeholder='Quiz Title'
+                  required=''
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor='description'
+                  className='block mb-2 text-md font-medium text-gray-900 dark:text-white'>
+                  Description
+                </label>
+                <textarea
+                  type='text'
+                  name='description'
+                  id='description'
+                  placeholder='Quiz Description'
                   className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
                   required=''
                 />
               </div>
-              <div className='relative w-full'>
-                <label
-                  htmlFor='reward'
-                  className='block mb-2 text-md font-medium text-gray-900 dark:text-white'>
-                  Tokens for Reward
-                </label>
-                <div className='flex absolute inset-y-0 right-8 top-7 items-center pl-3 pointer-events-none'>
-                  Ⓝ
+              <div className='flex gap-4 w-full'>
+                <div className='w-full'>
+                  <label
+                    htmlFor='winners'
+                    className='block mb-2 text-md font-medium text-gray-900 dark:text-white'>
+                    Number of Winners
+                  </label>
+                  <input
+                    type='number'
+                    min={0}
+                    name='winners'
+                    id='winners'
+                    placeholder='5'
+                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
+                    required=''
+                  />
                 </div>
-                <input
-                  type='number'
-                  min={0}
-                  name='reward'
-                  id='reward'
-                  placeholder='2'
-                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
-                  required=''
-                />
+                <div className='relative w-full'>
+                  <label
+                    htmlFor='reward'
+                    className='block mb-2 text-md font-medium text-gray-900 dark:text-white'>
+                    Reward per Winner
+                  </label>
+                  <div className='flex absolute inset-y-0 right-8 top-7 items-center pl-3 pointer-events-none'>
+                    Ⓝ
+                  </div>
+                  <input
+                    type='number'
+                    min={0}
+                    name='reward'
+                    id='reward'
+                    placeholder='2'
+                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
+                    required=''
+                  />
+                </div>
               </div>
-            </div>
-            <div className='flex items-center mb-4'>
-              <input
-                id='retake'
-                type='checkbox'
-                className='w-4 h-4 text-primary-600 bg-gray-100 rounded border-gray-300 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
-              />
-              <label
-                htmlFor='retake'
-                className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'>
-                Participants can retake the quiz multiple times
-              </label>
-            </div>
-          </form>
-        </div>
-        {/* Right Section */}
-        <div className='flex flex-col items-center justify-center text-left px-6 py-8 mx-auto w-full lg:p-0'>
-          <h2 className='mb-3 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
-            Configure Questions
-            {/* <button
+              <div className='flex items-center mb-4'>
+                <input
+                  id='retake'
+                  type='checkbox'
+                  className='w-4 h-4 text-primary-600 bg-gray-100 rounded border-gray-300 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+                />
+                <label
+                  htmlFor='retake'
+                  className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'>
+                  Participants can retake the quiz multiple times
+                </label>
+              </div>
+            </form>
+          </div>
+          {/* Right Section */}
+          <div className='flex flex-col items-center justify-center text-left px-6 py-8 mx-auto w-full lg:p-0'>
+            <h2 className='mb-3 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
+              Configure Questions
+              {/* <button
               className='h-8 w-8 text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium  rounded-full text-sm p-2.5 text-center inline-flex items-center justify-center ml-2 dark:bg-green-500 dark:hover:bg-green-600 dark:focus:ring-green-800'
               onClick={(e) => {
                 e.preventDefault();
@@ -120,45 +133,58 @@ const CreateQuiz = () => {
               }}>
               <span className='text-lg'>-</span>
             </button> */}
-          </h2>
-          <form
-            className='space-y-2 md:space-y-4 w-full'
-            action='#'
-            autoComplete='false'>
-            <div className='flex flex-col gap-4'>
-              {Array.from(Array(questions), (e, i) => {
-                return <AddQuestion key={i} n={i + 1} />;
-              })}
+            </h2>
+            <form
+              className='space-y-2 md:space-y-4 w-full'
+              action='#'
+              autoComplete='false'>
+              <div className='flex flex-col gap-4'>
+                {quizQuestions.map((question, index) => {
+                  return (
+                    <AddQuestion key={index} id={question.id} n={index + 1} />
+                  );
+                })}
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setQuizQuestions([
+                      ...quizQuestions,
+                      {
+                        id: quizQuestions.length + 1,
+                        question: '',
+                        options: [
+                          { id: uuidv4(), option: '', isCorrect: true },
+                          { id: uuidv4(), option: '', isCorrect: false },
+                        ],
+                      },
+                    ]);
+                  }}
+                  className='text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-md text-md p-2.5 text-center inline-flex items-center justify-center dark:bg-green-500 dark:hover:bg-green-600 dark:focus:ring-green-800'>
+                  Add a new question
+                </button>
+              </div>
               <button
+                className='mx-auto flex items-center bg-blue-500 hover:bg-blue-700 text-white text-xl font-semibold py-2 px-4 rounded'
                 onClick={(e) => {
                   e.preventDefault();
-                  setQuestions(questions + 1);
-                }}
-                className='text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-md text-md p-2.5 text-center inline-flex items-center justify-center dark:bg-green-500 dark:hover:bg-green-600 dark:focus:ring-green-800'>
-                Add a new question
+                  console.log(quizQuestions);
+                }}>
+                Submit
+                <svg
+                  className='ml-2 -mr-1 w-5 h-5'
+                  fill='currentColor'
+                  viewBox='0 0 20 20'
+                  xmlns='http://www.w3.org/2000/svg'>
+                  <path
+                    fillRule='evenodd'
+                    d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
+                    clipRule='evenodd'></path>
+                </svg>
               </button>
-            </div>
-            <button
-              className='mx-auto flex items-center bg-blue-500 hover:bg-blue-700 text-white text-xl font-semibold py-2 px-4 rounded'
-              onClick={(e) => {
-                e.preventDefault();
-                console.log();
-              }}>
-              Submit
-              <svg
-                className='ml-2 -mr-1 w-5 h-5'
-                fill='currentColor'
-                viewBox='0 0 20 20'
-                xmlns='http://www.w3.org/2000/svg'>
-                <path
-                  fillRule='evenodd'
-                  d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
-                  clipRule='evenodd'></path>
-              </svg>
-            </button>
-          </form>
+            </form>
+          </div>
         </div>
-      </div>
+      </QuestionsContext.Provider>
     </main>
   );
 };
